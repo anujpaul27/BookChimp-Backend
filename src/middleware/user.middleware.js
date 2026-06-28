a0bead9e5d996e60c41746368c2c584ba0874d7a
anuj paul <paulanuj27@gmail.com>
2026-06-28 22:44:43 +0600

    Verify user token

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,18 +1,31 @@
+const { default: mongoose } = require("mongoose");
 
-const verifyToken = async (req,res,next) =>
-{
+
+const verifyToken = async (req, res, next) => {
+  try {
     const token = req.headers.authorization;
-    console.log(token);
 
-    if (!token)
-    {
-        return res.status(400).json({
-            message: 'User token not found!.'
-        })
+    if (!token) {
+      return res.status(400).json({
+        message: "User token not found!.",
+      });
     }
 
-    next()
-}
+    // get session
+    const sessionModel = await mongoose.connection.db.collection("session");
+    const findSessionWithToken = await sessionModel.findOne({ token: token });
+    if (!findSessionWithToken) {
+      return res.status(403).json({
+        message: "Token invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {verifyToken}
-    
+
